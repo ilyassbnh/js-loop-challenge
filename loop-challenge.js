5fae8eef28d159e30c6ebf867e872b739a092d92
ilyassbnh <benhida.ilyass@gmail.com>
2025-10-22 14:35:41 +0100

    pushing the first 6 loop challenges

---
+++ loop-challenge.js
@@ -0,0 +1,34 @@
+//-----------challenge 1 --------------
+// let i = 1;
+// while (i <= 10){
+//     console.log(i)
+//     i++;
+// }
+//-----------challenge 2 --------------
+// for ( let i=1; i<=20;i++){
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+//-----------challenge 3 --------------
+// let i=0;
+// let sum=0;
+// while(i < 10){
+//     i++;
+//     sum += i;
+// }console.log(sum)
+//-----------challenge 4 --------------
+// let fruits = ["pomme","banane","orange","fraise"]
+// for(let i = 0 ; i < fruits.length; i++){
+//     console.log(fruits[i])
+//  }
+//-----------challenge 5 --------------
+// let mot = "frontend"
+// console.log(mot.length)
+//-----------challenge 6 --------------
+let table =["1","2","3","4","5"]
+let sum = ""
+for (let i =0;i<table.length;i++ ){
+    console.log("le nombre "+table[i]+ " multiplie par 2 est: "+ table[i]*2)
+}
+
