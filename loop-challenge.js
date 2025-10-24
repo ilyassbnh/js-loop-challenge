470064bf09a518975a304af7d1275d0300b0c547
ilyassbnh <benhida.ilyass@gmail.com>
2025-10-24 11:11:57 +0100

    minimum number from array challenge

--- loop-challenge.js
+++ loop-challenge.js
@@ -56,4 +56,26 @@
 // }
 // -----------challenge 11 --------------
 
+// function positiveSum(arr) {
+//   let sum = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       sum += arr[i]
+//     }
+//   }
+//   return sum
+// 
+
+// 
+let arr = [83, 0, -12, 95]
+let min = arr[0]
+for (let i = 0; i < arr.length; i++) {
+
+    if (min > arr[i]) {
+        min = arr[i]
+    }
+} console.log(min);
+
+
+
 
